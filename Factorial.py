1959150ca20455316551fc086557ca4d7eea0dd4
Samruddhi V Muni <samruddhimuni@gmail.com>
2026-01-11 20:59:44 +0530

    Create Factorial.py

---
+++ Factorial.py
@@ -0,0 +1,7 @@
+# 1. (C)Write a python program to find factorial of the given number? 
+ 
+num=int(input('enter the number :'))
+fact=1 
+for i in range(1,num+1):  
+    fact=fact*i 
+print(f"The Factorial of {num} is:",fact)
